5fa5e87f1bb8950f6728dad40afe54214af0f851
bilel <b.rehahla@it-akademy.fr>
2020-12-02 00:59:15 +0100

    crud in progress ->(create, update)

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -7,8 +7,11 @@ use App\Repository\PropertyRepository;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+//use Symfony\Component\Form\FormTypeInterface;
+//use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 Class AdminPropertyController extends AbstractController
 {
@@ -35,13 +38,36 @@ Class AdminPropertyController extends AbstractController
     public function index()
     {
         $properties = $this->repository->findAll();
-        //var_dump($properties);exit();
         return $this->render('admin/property/index.html.twig', compact('properties'));
+    }
+
+    /**
+     * @Route("/admin/property/create", name="admin.property.new")
+     * @param Property $property
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function new(Request $request){
+        $property = new Property();
+        $form= $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($property);
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
     }
 
     /**
      * @Route("/admin/{id}", name="admin.property.edit")
      * @param Property $property
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function edit(Property $property, Request $request){
