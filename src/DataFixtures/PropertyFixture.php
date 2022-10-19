6985b2225d407a6665da7c5b5370a501f3c29041
bilel <b.rehahla@it-akademy.fr>
2022-10-19 15:46:45 +0200

    Update symfony 4.4 to 5.4 and change swiftmailer to mailer

--- src/DataFixtures/PropertyFixture.php
+++ src/DataFixtures/PropertyFixture.php
@@ -4,27 +4,27 @@ namespace App\DataFixtures;
 use App\Entity\Property;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
+//use Faker\Factory;
 
 class PropertyFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('fr_FR');
+       // $faker = Factory::create('fr_FR');
         for ($i = 0; $i < 100; $i++ ){
             $property = new Property();
             $property
-                ->setTitle($faker->words(3, true))
-                ->setDescription($faker->sentences(3, true))
-                ->setSurface($faker->numberBetween(2,10))
-                ->setRooms($faker->numberBetween(1,9))
-                ->setBedrooms($faker->numberBetween(1,9))
-                ->setFloor($faker->numberBetween(0,15))
-                ->setPrice($faker->numberBetween(10000,1000000))
-                ->setHeat($faker->numberBetween(0,count(Property::HEAT) - 1))
-                ->setCity($faker->city)
-                ->setAddress($faker->address)
-                ->setPostalCode($faker->postcode)
+                ->setTitle("maison54")
+                ->setDescription("loream ipsum")
+                ->setSurface("5")
+                ->setRooms("5")
+                ->setBedrooms("5")
+                ->setFloor("3")
+                ->setPrice("1500000")
+                ->setHeat("1")
+                ->setCity("lyon")
+                ->setAddress("4 rue victor hugo")
+                ->setPostalCode("69000")
                 ->setSold(false);
              $manager->persist($property);
         }
